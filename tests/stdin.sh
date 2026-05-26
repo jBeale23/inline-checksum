c0d2ec99bfe71d620c8f73c76055840818c704e8
Josh Beale <jbeale2023@gmail.com>
2026-05-26 12:15:50 -0400

    Added test that inline-checksum behaves as expected while processing stdin

---
+++ tests/stdin.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+# ------------------------------------- #
+#             Return Codes              #
+# ------------------------------------- #
+# 0: All Algorithms Pass with Stdin     #
+# 1: Missing sha256sum                  #
+# 2: Missing sha512sum                  #
+# 3: Missing md5sum                     #
+# 4: Failed on sha256sum Check          #
+# 5: Failed on sha512sum Check          #
+# 6: Failed on md5sum Check             #
+# ------------------------------------- #
+
+export wkdir="$(CDPATH="" cd -- "$(dirname -- "${0}")" && pwd -P)"
+. "${wkdir}/shared_utils" || return "${?}"
+
+for algorithm in "sha256" "sha512" "md5"; do
+	correct_hash_pointer=$(printf "correct_%s" "${algorithm}")
+	eval "correct_hash=\$${correct_hash_pointer}"
+	return_code_pointer=$(printf "return_%s" "${algorithm}")
+	eval "return_code=\$${return_code_pointer}"
+	cat "${license_file}" | "${script}" --algorithm="${algorithm}" "${correct_hash}" - > /dev/null
+	case "${?}" in
+		0) ;;
+		*)
+			printf "[%s]: Failed stdin test with algorithm '%s'.\n" "$(date +'%H:%M:%S')" "${algorithm}" >&2
+			return "${return_code}"
+			;;
+	esac
+done
