5533579088cdb93830bf24af9db0e1b0f2b32381
Josh Beale <jbeale2023@gmail.com>
2026-05-28 07:52:50 -0400

    Added tests for failure when providing a file that does not match the correct hash

---
+++ tests/incorrect_hash_stdin.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+# ------------------------------------- #
+#             Return Codes              #
+# ------------------------------------- #
+# 0: All Algorithms Fail                #
+# 1: Missing sha256sum                  #
+# 2: Missing sha512sum                  #
+# 3: Missing md5sum                     #
+# 4: Failed on sha256sum Check          #
+# 5: Failed on sha512sum Check          #
+# 6: Failed on md5sum Check             #
+# ------------------------------------- #
+
+export wkdir="$(CDPATH="" cd -- "$(dirname -- "${0}")" && pwd -P)"
+. "${wkdir}/shared_utils" || return "${?}"
+
+for algorithm in "sha256" "sha512" "md5"; do
+	correct_hash_pointer=$(printf "correct_%s" "${algorithm}")
+	eval "correct_hash=\$${correct_hash_pointer}"
+	truncated_hash="${correct_hash}"
+	while [ ${#truncated_hash} -gt 16 ]; do
+    truncated_hash="${truncated_hash%?}"
+	done
+	return_code_pointer=$(printf "return_%s" "${algorithm}")
+	eval "return_code=\$${return_code_pointer}"
+	cat "${script}" | "${script}" --algorithm="${algorithm}" "${truncated_hash}" - > /dev/null 2>&1
+	case "${?}" in
+		1);;
+		*)
+			printf "[%s]: Failed incorrect hash from stdin test with algorithm '%s'.\n" "$(date +'%H:%M:%S')" "${algorithm}" >&2
+			return "${return_code}"
+			;;
+	esac
+done
